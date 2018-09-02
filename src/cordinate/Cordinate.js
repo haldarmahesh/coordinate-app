499b666f176500d219147164eedfd994605565f2
Mahesh Haldar <haldar.mahesh@gmail.com>
2018-09-02 18:21:30 +0400

    Return exception and set default values

--- src/cordinate/Cordinate.js
+++ src/cordinate/Cordinate.js
@@ -1,8 +1,13 @@
 export default class Coordinate {
-    constructor(x, y) {
+    constructor(x = 0, y = 0) {
+        if(arguments.length > 2) {
+            throw new Error('Please pass 2 or less parameters.');
+        }
+
         this.x = x;
         this.y = y;
     }
+
     toString() {
         return `{${this.x},${this.y}}`;
     }
--- src/cordinate/cordinate.test.js
+++ src/cordinate/cordinate.test.js
@@ -5,13 +5,32 @@ describe('Cordinate setup', () => {
         expect(cordinate1.x).toEqual(10);
         expect(cordinate1.y).toEqual(20);
     });
-    test('should store the first two params in x and y', () => {
-        let cordinate1 = new Cordinate(10, 20, 30);
-        expect(cordinate1.x).toEqual(10);
-        expect(cordinate1.y).toEqual(20);
+    test('should throw error when more than 2 cordinates are passed', () => {
+        let cordinate1 = () => new Cordinate(10, 20, 30);
+        expect(cordinate1).toThrowError();
+    });
+    test('should set param to 0 if provided without any params', () => {
+        let cordinate1 = new Cordinate();
+        expect(cordinate1.x).toEqual(0);
+        expect(cordinate1.y).toEqual(0);
+    });
+    test('should set 0 to y if only one param is passed', () => {
+        let cordinate1 = new Cordinate(12);
+        expect(cordinate1.x).toEqual(12);
+        expect(cordinate1.y).toEqual(0);
     })
     test('should return cordinated with curly braces when toString is called', () => {
         let cordinate1 = new Cordinate(10, 20);
         expect(cordinate1.toString()).toEqual(`{10,20}`);
-    })
+    });
+});
+
+describe('Override + operator to add Cordinate obj', () => {
+  test('should return the added cordinates when Obj is added', () => {
+      let point1 = new Cordinate(1, 1);
+      let point2 = new Cordinate(2, 2);
+      let addedPoint = new Cordinate(point1 + point2);
+      expect(addedPoint.x).toEqual(3);
+      expect(addedPoint.y).toEqual(3);
+  })
 })
