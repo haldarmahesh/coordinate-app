34842c66399644347ff3362e45321c426b1ae92a
Mahesh Haldar <haldar.mahesh@gmail.com>
2018-09-03 12:11:14 +0400

    Add basic component with buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Coordinate from './coordinate/Coordinate';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isCoordinateAdded: false
+    };
+  }
+
+  handleNewCoordinate() {
+    this.setState({
+      isCoordinateAdded: true
+    });
+  }
+
+  handleNewCoordinatePoint() {
+    this.setState({});
+  }
+
+  renderCoordinate() {
+    const { isCoordinateAdded } = this.state;
+    return isCoordinateAdded ? 'new Coordinate(..)' : null;
+  }
+
+  renderCreateNewPoint() {
+    const { isCoordinateAdded } = this.state;
+    const component = (
+      <div className="row mt-2">
+        <div className="col-md-4">
+          Add new Coordinate points as argument
+        </div>
+        <div className="col-md-2">
+          <div className="row">
+            <div className="col-md-12">
+
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">X</span>
+                </div>
+                <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
+              </div>
+            </div>
+            <div className="col-md-12">
+
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">Y</span>
+                </div>
+                <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="new-coordinate-point-button-container">
+                <button
+                  className="btn btn-warning btn-block"
+                  type="button"
+                  onClick={() => this.handleNewCoordinatePoint()}
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+    return isCoordinateAdded ? component : null;
+  }
+
   render() {
-    const point1 = new Coordinate(1, 1);
-    const point2 = new Coordinate(2, 2);
-    const addedPoint = new Coordinate(point1 + point2);
-    console.log(addedPoint);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            Start by creating new Coordinate
+          </div>
+          <div className="col-md-2">
+            <div className="new-coordinate-button-container">
+              <button
+                className="btn btn-primary btn-block"
+                type="button"
+                onClick={() => this.handleNewCoordinate()}
+              >
+                Create
+              </button>
+            </div>
+          </div>
+        </div>
+        {this.renderCreateNewPoint()}
+        <div className="row">
+          <div className="col-md-8">
+            {this.renderCoordinate()}
+          </div>
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component setup', () => {
+  test('should start with false CoordinateAdded', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().isCoordinateAdded).toEqual(false);
+  });
+  test('should return null when isCoordinate is false and renderCoordinte is called', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.instance().renderCoordinate()).toEqual(null);
+  });
+  test('should return some text when isCoordinate is true and renderCoordinte is called', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({
+      isCoordinateAdded: true
+    });
+    expect(typeof wrapper.instance().renderCoordinate()).toEqual('string');
+  });
+  test('should set the state value isCoordinateAdded to true when handle is called', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().isCoordinateAdded).toEqual(false);
+    wrapper.instance().handleNewCoordinate();
+    expect(wrapper.state().isCoordinateAdded).toEqual(true);
+  });
+  test('should return null when renderCreateNew is called when coordinate is not added', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.instance().renderCreateNewPoint()).toEqual(null);
+  });
+  test('should return object when renderCreateNew is called when coordinate is added', () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().handleNewCoordinate();
+    expect(typeof wrapper.instance().renderCreateNewPoint()).toEqual('object');
+  });
 });
